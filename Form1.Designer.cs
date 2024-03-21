e4a6510de44d1fd7163011b2d336e8e306ed7c4b
WANG <wangqianlf@live.com>
2024-03-21 22:36:06 +0100

    arguments des évènements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 77);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 77);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(551, 65);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
@@ -36,6 +79,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1188, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mon programme";
@@ -47,6 +94,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,7 @@ namespace WinForms_Fenetre_notes_cours
         private void Form1_Load(object sender, EventArgs e)
         {
             //MessageBox.Show("Bienvenue au programme !");
+            MessageBox.Show(sender.ToString());
         }
 
         private void Form1_Click(object sender, EventArgs e)
@@ -30,7 +31,24 @@ namespace WinForms_Fenetre_notes_cours
 
         private void Form1_ResizeEnd(object sender, EventArgs e)
         {
-            MessageBox.Show("Fin du redimentionnement de la fenêtre !");
+            //MessageBox.Show("Fin du redimentionnement de la fenêtre !");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
+            //label1.Text = button1.Text;
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
+            label1.Text = ((Button)sender).Text;
+            //MessageBox.Show(e.ToString());
+            PointF mousePosition = (e as MouseEventArgs).Location;
+            MessageBox.Show(mousePosition.ToString());
+            //label1.Text += " X: " + mousePosition.X + " Y: " + mousePosition.Y;
         }
     }
 }
